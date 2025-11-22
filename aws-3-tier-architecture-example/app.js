39d5a797513460027bda7f8dc856a0e10b349a88
Kim Jun Young <rlawnsdud0422@gmail.com>
2025-11-22 10:56:42 +0900

    updated Example

--- aws-3-tier-architecture-example/app.js
+++ aws-3-tier-architecture-example/app.js
@@ -20,7 +20,7 @@ const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
 }
 
 let pool
